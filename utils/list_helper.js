5de01cbfa8211d774bd35c9e4d79e5a10f6704c1
Jussi <jussi.kanto@tuni.fi>
2022-09-08 13:21:31 +0300

    Add 4.5 - apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -22,7 +22,29 @@ const totalLikes = (blogs) => {
   }
 };
 
+const favoriteBlog = (blogs) => {
+  let mostLikes = 0;
+  let fvrtBlog = '';
+
+  if (blogs.length === 0) {
+    return 0;
+  } else {
+    blogs.map((blog) => {
+      if (blog.likes > mostLikes) {
+        mostLikes = blog.likes;
+        fvrtBlog = {
+          title: blog.title,
+          author: blog.author,
+          likes: blog.likes,
+        };
+      }
+    });
+  }
+  return fvrtBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
