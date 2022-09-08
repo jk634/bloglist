3b73b2f31a16af5f8cd6f217e79fc7ed057c8925
Jussi <jussi.kanto@tuni.fi>
2022-09-08 12:22:18 +0300

    Add 4.4 - apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,27 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  let likes = 0;
+  let likesIsWrongType = false;
+
+  if (blogs.length === 0) {
+    return 0;
+  } else {
+    blogs.map((blog) => {
+      if (typeof blog.likes !== 'number') {
+        likesIsWrongType = true;
+        return;
+      } else {
+        likes += blog.likes;
+      }
+    });
+    if (likesIsWrongType) return null;
+    return likes;
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
