f905f126f92374fba306298615fda734b13e15fa
Jussi <jussi.kanto@tuni.fi>
2022-09-15 11:53:23 +0300

    Add 4.19 - blogilistan laajennus, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,37 +1,55 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => {
-  let body = request.body;
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    let body = request.body;
 
-  if (!body.likes) {
-    request.body.likes = 0;
+    if (!body.likes) {
+      request.body.likes = 0;
+    }
+    if (!body.title && !body.url) {
+      response.status(400).end();
+      return;
+    }
+
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' });
+    }
+    const user = await User.findById(decodedToken.id);
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id,
+    });
+
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+    response.status(201).json(savedBlog);
+  } catch (err) {
+    next(err);
   }
-  if (!body.title && !body.url) {
-    response.status(400).end();
-    return;
-  }
-
-  const user = await User.findById(body.userId);
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id,
-  });
-
-  const savedBlog = await blog.save();
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
-  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
