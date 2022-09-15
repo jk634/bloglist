451979d16d637b4d2fe968b05619fb885b4d9bec
Jussi <jussi.kanto@tuni.fi>
2022-09-15 12:09:06 +0300

    Add 4.20 - blogilistan laajennus, step8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
@@ -28,7 +20,7 @@ blogsRouter.post('/', async (request, response, next) => {
       return;
     }
 
-    const token = getTokenFrom(request);
+    const token = request.token;
     const decodedToken = jwt.verify(token, process.env.SECRET);
     if (!token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' });
